8afff1de6f41604a049e806b683fb21fb2e8e3d3
farrelinoarv <eyesbergice@gmail.com>
2025-11-09 00:34:47 +0700

    Add CI/CD pipeline for ruang-app-backend

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// for testing
+Route::get('/test-ci-cd', function () {
+    return 'This is a test route!';
+});
